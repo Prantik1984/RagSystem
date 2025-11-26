13b1296407090fe0abb3f7823259534bd199a4d1
Prantik <prantik.sarmah84@gmail.com>
2025-11-26 20:00:04 +0530

    updating

--- WebSiteSummarizer/Chunker.py
+++ WebSiteSummarizer/Chunker.py
@@ -1,9 +1,17 @@
-# from newspaper import Article
-#
-# class Chunker:
-#     def chunk_text(self,article:Article):
-#       """"
-#       creates chunks of text from article
-#       """
-#         return article.text
+from newspaper import Article
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+class Chunker:
+    def chunk_text(self,article:Article):
+      """"
+      creates chunks of text from article
+      """
+      article_text=article.text
+
+      text_splitter = RecursiveCharacterTextSplitter(
+          chunk_size=500,
+          chunk_overlap=100
+      )
+
+      texts = text_splitter.split_text(article_text)
+      return texts
 
--- WebSiteSummarizer/Main.py
+++ WebSiteSummarizer/Main.py
@@ -1,11 +1,14 @@
 from langchain_community.chat_models import ChatOllama
 from WebOperator import WebOperator
-
+from ..QuerringTextFiles.Embedder import Embedder
+from Chunker import Chunker
 def Main():
     site_link="https://www.artificialintelligence-news.com/news/alibaba-qwen-ai-app-10-million-downloads/"
     webOperator = WebOperator()
     article=webOperator.download_article(site_link)
-    print(article)
 
+    chunk_creator= Chunker()
+    chunked_texts= chunk_creator.chunk_text(article)
+    print(chunked_texts)
 if __name__ == "__main__":
     Main()
